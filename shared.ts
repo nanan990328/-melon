d5e100d49ed72b90016dc2f99e0df6ec63e84148
nanan99 <nanan990328@gmail.com>
2026-04-21 16:18:19 +0800

    Add shared constants and utility functions for automation

---
+++ shared.ts
@@ -0,0 +1,64 @@
+import type { Page } from "puppeteer"
+
+/**
+ * The paths to your JSON file for cookies.
+ */
+export const COOKIES_JSON = "./cookies.json"
+
+/**
+ * If false, a Chromium tab will open, and you'll be able to see the automation run.
+ */
+export const IS_HEADLESS: boolean = false
+
+/**
+ * The link to the Melon Global login page.
+ */
+export const LOGIN_URL: string =
+  "https://gmember.melon.com/login/login_form.htm?langCd=EN"
+
+/**
+ * The link to your target concert, on the Melon Global website.
+ */
+export const CONCERT_URL =
+  "https://tkglobal.melon.com/performance/index.htm?langCd=EN&prodId=213025"
+
+/**
+ * Sometimes, concerts run over multiple days.
+ * For which of these days are you looking for tickets?
+ * 0-indexed number.
+ */
+export const NIGHT: number = 0
+
+/**
+ * It's typically unlikely for the best tickets (for example, VIP/floor) to be found.
+ * You can forego checking these sections.
+ */
+export const CHECK_BEST: boolean = true
+
+/**
+ * How many times should it check through every section, before giving up?
+ */
+export const LOOPS: number = 9999
+
+/**
+ * The YouTube video to play when a ticket is found.
+ */
+export const YOUTUBE_VIDEO: string =
+  "https://www.youtube.com/watch?v=Ngpf6UtPn4k"
+
+export const loadBrowser = async (page: Page, WEBSITE_URL: string) => {
+  await page.goto(WEBSITE_URL), { 
+    waitUntil: 'domcontentloaded', 
+    timeout: 60000 
+  }
+
+  await page.setViewport({ width: 1080, height: 1024 })
+
+  await page.waitForNetworkIdle()
+}
+
+export const delay = async (time: number) => {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  })
+}
