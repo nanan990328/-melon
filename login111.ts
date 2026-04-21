bc1078caaf381de5815f2df238790530a2a2cac5
nanan99 <nanan990328@gmail.com>
2026-04-21 16:22:39 +0800

    Add login and cookie saving functionality
    Implement login functionality with cookie saving.

---
+++ login111.ts
@@ -0,0 +1,34 @@
+import fs from "fs"
+import puppeteer from "puppeteer"
+
+import CREDENTIALS from "./credentials.json"
+import { COOKIES_JSON, IS_HEADLESS, LOGIN_URL, loadBrowser } from "./shared"
+
+const saveCookies = async () => {
+  try {
+    const cookies = await page.cookies()
+
+    fs.writeFileSync(COOKIES_JSON, JSON.stringify(cookies, null, 2))
+    console.log("Saved cookies to", COOKIES_JSON)
+  } catch (error) {
+    console.error("Error saving cookies:", error)
+  }
+}
+
+const logIn = async () => {
+  await page.locator("#email").fill(CREDENTIALS.email)
+  await page.locator("#pwd").fill(CREDENTIALS.password)
+  await page.locator("#formSubmit").click()
+
+  await page.waitForNetworkIdle()
+}
+
+const browser = await puppeteer.launch({ headless: IS_HEADLESS })
+const page = await browser.newPage()
+
+await loadBrowser(page, LOGIN_URL)
+
+await logIn()
+await saveCookies()
+
+await browser.close()
